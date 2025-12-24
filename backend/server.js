1735f8f907a505b4557438999cd7948fdb21944d
Khalid ghlaib <khalidghalib426@gmail.com>
2025-12-24 15:56:52 +0430

    the helmet midileware is implemented

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express"
-
+import helmet from "helmet"
+import morgan from "morgan"
 
 const app = express();
+app.use(helmet());
+
+app.get("/test", (req, res) => {
+  console.log(res.getHeaders())
+  res.send("Hello World");
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
