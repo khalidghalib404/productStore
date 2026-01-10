2117bd386918f6a54cbf10f2204726846e1b7cd3
Khalid ghlaib <khalidghalib426@gmail.com>
2026-01-10 23:16:47 +0430

    the  db configed

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,8 @@ async  function  initDB(){
 initDB().then(()=>{
   app.listen(PORT, () => {
     console.log("Server is running on port " + PORT  );
+    console.log("DB connected successfully")
+    
   })
 })
 
