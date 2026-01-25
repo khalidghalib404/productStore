772cb6ef7613e6fade6dd96f21d05aae10f7896c
Khalid ghlaib <khalidghalib426@gmail.com>
2026-01-25 21:47:15 +0430

    adding browser router for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react'
+import Navbar from './components/navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import ProductPage from './pages/ProductPage'
  
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <div>
-     <h1> hey  </h1>
-     <div>
-      <h3 className=' text-red-500'>the header</h3>
-      
-     </div>
-
-  
+    <BrowserRouter>
+    
+    <div className='min-h-screen bg-base-200 transition-colors duration-300'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/product/:id' element={<ProductPage/>}/>
+      </Routes>
+      Hello World!
     </div>
+    </BrowserRouter> 
   )
 }
 
